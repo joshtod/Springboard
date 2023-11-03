2b8393ed6505e7d447961867b719ee140a3e97c7
joshtod <134558203+joshtod@users.noreply.github.com>
2023-11-03 16:46:35 -0500

    Kaggle housing project, work in progress for Springboard, initial upload.

---
+++ Kaggle Housing/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Kaggle project on advanced regression techniques, executed for Springboard.',
+    author='Joshua Ogden-Davis',
+    license='MIT',
+)
