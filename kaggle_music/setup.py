94e4f0e944a528e50c47823a0b91f00da23fb2c3
joshtod <134558203+joshtod@users.noreply.github.com>
2024-01-11 21:28:34 -0600

    Presentation assignment, first submission

---
+++ kaggle_music/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An exploration of a data set on album characteristics and review scores sourced from Kaggle',
+    author='Joshua Ogden-Davis',
+    license='MIT',
+)
